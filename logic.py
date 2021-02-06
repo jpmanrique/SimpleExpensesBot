4039050170bda2598c3c5bb11a9a4f35b99a9418
jpmanrique <johannap.manriquem@autonoma.edu.co>
2021-02-06 15:12:37 -0500

    se agrego /Listar ganancias  #17

--- logic.py
+++ logic.py
@@ -120,3 +120,19 @@ def spend_money(user_id, amount):
     db.session.commit()
     
     return True
+
+
+def list_earnings (user_id, month, year):
+    earnings = db.session.query(
+                Earning
+            ).filter_by(
+                accounts_id=user_id
+            ).filter(
+                extract('month', Earning.when) == month
+            ).filter(
+                extract('year', Earning.when) == year
+            ).all()
+    
+    db.session.commit()
+        
+    return earnings
