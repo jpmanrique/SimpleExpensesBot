84aa709cd051993946c73f4fb8d7969a73299a00
jpmanrique <johannap.manriquem@autonoma.edu.co>
2021-02-06 12:09:39 -0500

    version inicial bot

--- bot.py
+++ bot.py
@@ -5,6 +5,10 @@ import config
 from time import sleep
 import re
 import logic
+import database.db as db
+
+if __name__ == '__main__':
+    db.Base.metadata.create_all(db.engine)
 
 #########################################################
 
@@ -18,6 +22,40 @@ def on_command_help(message):
     pass
 
 
+@bot.message_handler(commands=['about'])
+def on_command_about(message):
+    pass
+
+
+@bot.message_handler(regexp=r"^(gane|gané|g) ([+-]?([0-9]*[.])?[0-9]+)$")
+def on_earn_money(message):
+    pass
+
+
+@bot.message_handler(regexp=r"^(gaste|gasté|gg) ([+-]?([0-9]*[.])?[0-9]+)$")
+def on_spend_money(message):
+    pass
+
+
+@bot.message_handler(regexp=r"^(listar ganancias|lg) en ([0-9]{1,2}) de ([0-9]{4})$")
+def on_list_earnings(message):
+    pass
+
+
+@bot.message_handler(regexp=r"^(listar gastos|lgg) en ([0-9]{1,2}) de ([0-9]{4})$")
+def on_list_spendings(message):
+    pass
+
+
+@bot.message_handler(regexp=r"^(obtener saldo|s)$")
+def on_get_balance(message):
+    pass
+
+
+@bot.message_handler(regexp=r"^(remover|r) (ganancia|g|gasto|gg) ([0-9]+)$")
+def on_remove_record(message):
+    pass
+
 
 # Aquí vendrá la implementación de la lógica del bot
 
@@ -25,13 +63,8 @@ def on_command_help(message):
 
 @bot.message_handler(func=lambda message: True)
 def on_fallback(message):
-    bot.send_chat_action(message.chat.id, 'typing')
-    sleep(1)
+    pass
 
-    bot.reply_to(
-        message,
-        "\U0001F63F Ups, no entendí lo que me dijiste.")
-        
 
 if __name__ == '__main__':
         bot.polling(timeout=20)
