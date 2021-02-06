778c5bdd372e6b6d458164adb84b03e83f0aca6f
jpmanrique <johannap.manriquem@autonoma.edu.co>
2021-02-06 14:54:25 -0500

    se agrego /Gastar dinero  #16

--- logic.py
+++ logic.py
@@ -99,3 +99,24 @@ def update_account (user_id, amount):
     db.session.commit()
     
     return True
+
+
+def spend_money(user_id, amount):
+    if amount <= 0:
+        return False
+    
+    control = update_account(user_id, amount * -1)
+    
+    if not control:
+        return False
+
+    spend = Spending(
+          amount,
+          datetime.now(),
+          user_id)
+    
+    db.session.add(spend)
+    
+    db.session.commit()
+    
+    return True
