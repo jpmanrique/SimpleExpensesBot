0dda29404007748512648662705bd896b8e5b05b
jpmanrique <johannap.manriquem@autonoma.edu.co>
2021-02-06 12:27:49 -0500

    se agrego /about #9

---
+++ logic.py
@@ -0,0 +1,7 @@
+def get_about_this(VERSION):
+    response = (
+                f"Simple Expenses Bot (pyTelegramBot) v{VERSION}"
+                "\n\n"
+                "Desarrollado por Jorge I. Meza <jimezam@autonoma.edu.co>"
+                )
+    return response
